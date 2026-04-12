991d09163d6f2c2c53f42dd63e68d6054a0dd443
tupciohrupcio <olekbrulinski@gmail.com>
2026-04-12 20:09:37 +0200

    Add files via upload

---
+++ Brulinski_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("Mój numer w dzienniku to 1"+"<br>");
+document.write("Pierwszy wiersz to:   16     Tak Nie 6 "+"<br>");
+document.write("Drugi wiersz to:          2     Tak Nie 6 "+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("aqua").bold().fontsize(6)+"<br>"); 
+document.write(document.lastModified.fontcolor("navy").bold().fontsize(6)+"<br>");
